7a0791a0abe529f4dbde23cf388ef9439748cd55
Kirill <mustang.horse34@gmail.com>
2024-03-09 21:10:52 +0700

    add API

---
+++ src/API/getGroups.ts
@@ -0,0 +1,56 @@
+import { GetGroupsParams } from 'src/types';
+
+import groupsJSON from 'src/groups.json';
+
+const getGroups = async ({ type, color, hasFriends }: GetGroupsParams) => {
+  try {
+    const promise = new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const res = await promise
+      .then(() => {
+        return {
+          result: 1,
+          data: groupsJSON
+            .filter((group) => {
+              return type === ''
+                ? true
+                : type === 'open'
+                ? group.closed === false
+                : group.closed === true;
+            })
+            .filter((group) => {
+              return color === 'all'
+                ? true
+                : color === group.avatar_color
+                ? true
+                : false;
+            })
+            .filter((group) => {
+              return hasFriends === ''
+                ? true
+                : hasFriends === 'yes'
+                ? group.friends && group.friends?.length > 0
+                : hasFriends === 'no'
+                ? !group.friends
+                : false;
+            }),
+        };
+      })
+      .catch((error) => {
+        throw new Error(`There was a error: ${error}`);
+      });
+
+    if (res.result == 0) throw new Error(`Result is 0`);
+    if (!res.data) throw new Error(`No data`);
+
+    return res;
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      throw new Error(`There was a SyntaxError: ${error}`);
+    } else {
+      throw new Error(`There was a error: ${error}`);
+    }
+  }
+};
+
+export default getGroups;
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Filters } from './Filters';
+export { default as Filter } from './Filter';
+export { default as Groups } from './Groups';
+export { default as Group } from './Group';
